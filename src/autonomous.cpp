efb87c9905a41581873ea6f8577f54b4842872ce
Tyler Newcomb <2019tylernewcomb@smba-54365.local>
2019-03-01 18:11:51 -0500

    code before scrim

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -34,28 +34,42 @@ void gyroadj(void* param) {
        gyroOutput = gyroOutput + diff;
      }
 
- 		 std::cout << gyro.get() << "    " << gyroOutput << std::endl;
-      pros::delay(1);
+ 		 //std::cout << gyro.get() << "    " << gyroOutput << std::endl;
+     pros::delay(1);
 
  	}
  }
 
 void turnPID (double target) {
-  MiniPID pid = MiniPID(0.21, 0.000, 0.5);
+  MiniPID pid = MiniPID(0.17, 0.000, 0.95);
   pid.setOutputLimits(-127, 127);
   pid.setMaxIOutput(30);
   pid.setSetpointRange(900);
-  int bias = 38;
+  int bias = 0;
+  //bias was 38
+
   //reset the gyro value
   //gyro.reset();
   gyroOutput = 0;
 
   //double target = 900;
   int iterations = 0;
+  /*
+  if(target > 1000) {
+    bias = -30;
+  } else if (target < -1000) {
+    bias = 85;
+  }
 
-  while(iterations < 1000) {
+  if(target <= 1000) {
+    bias = -0;
+  } else if (target >= -1000) {
+    bias = 85;
+  }
+  */
+  while(iterations < 1500) {
      //std::cout << driveRightFront.get_actual_velocity() << std::endl;
-     double output = pid.getOutput(gyroOutput / 1.075, target + bias);
+     double output = pid.getOutput(gyroOutput / 1.068, target + bias);
      driveRightFront.move(-output);
      driveRightBack.move(-output);
      driveLeftFront.move(output);
@@ -64,16 +78,20 @@ void turnPID (double target) {
      if(driveRightFront.get_actual_velocity() == 0 && iterations > 30) {
         break;
      }
-     //pros::lcd::print(0, "Gyro: %f\n", (gyro.get()) -900);
+     pros::lcd::print(0, "Gyro: %f\n", gyro.get());
      //pros::lcd::print(1, "PID : %f\n", output);
      iterations = iterations + 10;
 
 
   }
+  driveRightFront.move(0);
+  driveRightBack.move(0);
+  driveLeftFront.move(0);
+  driveLeftBack.move(0);
 }
 
 void turnPIDTime (double target, double time) {
-  MiniPID pid = MiniPID(0.25, 0.000, 0.4);
+  MiniPID pid = MiniPID(0.13, 0.000, 0.9);
   pid.setOutputLimits(-127, 127);
   pid.setMaxIOutput(30);
   pid.setSetpointRange(900);
@@ -110,24 +128,25 @@ void turnPIDTime (double target, double time) {
   driveLeftBack.move(0);
 }
 
-void drivePID (double target, double setPointRange = 900, double rightBias = 30) {
-  int leftBias = 7;
-  rightBias = 0;
+void drivePID (double target, double setPointRange = 900, double rightBias = 0) {
+  int leftBias = 0;
+  rightBias = 30;
+  /*
   if (target < 0) {
      leftBias = leftBias * -1;
      rightBias = rightBias * -1;
   }
-
-  MiniPID leftPID = MiniPID(0.28, 0.000, 0.55);
+  */
+  MiniPID leftPID = MiniPID(0.24, 0.000, 0.50);
   leftPID.setOutputLimits(-127, 127);
   leftPID.setMaxIOutput(30);
   leftPID.setSetpointRange(setPointRange);
 
+  MiniPID rightPID = MiniPID(0.24, 0.000, 0.50);
+  leftPID.setOutputLimits(-127, 127);
+  leftPID.setMaxIOutput(30);
+  leftPID.setSetpointRange(setPointRange);
 
-  MiniPID rightPID = MiniPID(0.265, 0.000, 0.55);
-  rightPID.setOutputLimits(-127, 127);
-  rightPID.setMaxIOutput(30);
-  rightPID.setSetpointRange(setPointRange);
 
 
 /*
@@ -150,11 +169,14 @@ void drivePID (double target, double setPointRange = 900, double rightBias = 30)
 
   //double target = 900;
   int iterations = 0;
-  float motorEncoderAverage;
+  float leftMotorEncoderAverage;
+  float rightMotorEncoderAverage;
+
   while(iterations < 4000) {
-     motorEncoderAverage = (driveRightFront.get_position() + driveRightBack.get_position() + driveLeftFront.get_position() + driveLeftBack.get_position()) / 4;
-     double leftOutput = leftPID.getOutput(motorEncoderAverage, target + leftBias);
-     double rightOutput = rightPID.getOutput(motorEncoderAverage, target + rightBias);
+     leftMotorEncoderAverage = (driveLeftFront.get_position() + driveLeftBack.get_position()) / 2;
+     rightMotorEncoderAverage = (driveRightFront.get_position() + driveRightBack.get_position()) / 2;
+     double leftOutput = leftPID.getOutput(leftMotorEncoderAverage, target + leftBias);
+     double rightOutput = 0.95 * rightPID.getOutput(rightMotorEncoderAverage, target + rightBias);
      driveRightFront.move(rightOutput);
      driveRightBack.move(rightOutput);
      driveLeftFront.move(leftOutput);
@@ -167,7 +189,9 @@ void drivePID (double target, double setPointRange = 900, double rightBias = 30)
 
 
   }
-  pros::lcd::print(1, "PID : %f\n", motorEncoderAverage);
+  //pros::lcd::print(1, "PID : %f\n", motorEncoderAverage);
+  std::cout << "Left Error: " << leftMotorEncoderAverage << std::endl;
+  std::cout << "Right Error: " << rightMotorEncoderAverage << std::endl;
 }
 
 void ultrasonicAlignPID () {
@@ -192,32 +216,36 @@ void ultrasonicAlignPID () {
 void red_front_auton() {
   //red primary auton
   // line up with the left side of the tile
-  drivePID(3200);
-  intake.move(-127);
-  pros::delay(100);
-  drivePID(-2400);
-  intake.move(0);
+  drivePID(3100);
+  intake.move(127);
+  pros::delay(400);
+  drivePID(-2300);
 
   turnPID(1800);
+  intake.move(0);
   catapult.move(127);
-  catapult2.move(127);
+  catapult2.move(-127);
   pros::delay(400);
   catapult.move(0);
   catapult2.move(0);
   turnPID(900+350);
-  intake.move(127);
-  drivePID(1000);
+  intake.move(-127);
+  drivePID(900);
   drivePID(810, 280);
   intake.move(0);
-  drivePID(-2030);
+  drivePID(-1900);
   pros::delay(500);
   turnPID(1400);
-  drivePID(1650);
+  drivePID(1900);
   turnPID(-900);
   drivePID(4000);
 }
 void red_back_auton() {
   //red back
+  turnPID(900);
+  pros::delay(1500);
+  turnPID(-900);
+  return;
   drivePID(3200);
   intake.move(-127);
   pros::delay(300);
@@ -250,63 +278,37 @@ void red_back_auton() {
 }
 void blue_front_auton() {
   //blue primary auton
-  drivePID(3200);
-  intake.move(-127);
+  drivePID(3100);
+  intake.move(127);
   pros::delay(100);
-  drivePID(-2850);
+  drivePID(-2700);
   intake.move(0);
   catapult.move(127);
-  catapult2.move(127);
+  catapult2.move(-127);
   pros::delay(400);
   catapult.move(0);
   catapult2.move(0);
-  turnPID(490);
-  intake.move(127);
-  drivePID(1100);
+  turnPID(600);
+  intake.move(-127);
+  drivePID(1250);
   drivePID(800, 250);
   intake.move(0);
-  drivePID(-1900);
-  turnPID(-1400);
+  drivePID(-1550);
+  turnPID(-1200);
   drivePID(1750);
   turnPID(900);
   drivePID(4000);
 }
 void blue_back_auton() {
   //Blue back
-  drivePID(3200);
-  intake.move(-127);
+
+  drivePID(3100);
+  intake.move(127);
   pros::delay(300);
   drivePID(-500);
-  turnPID(400);
   intake.move(0);
-  drivePID(-2250);
-  turnPID(-400);
-  driveRightFront.move(-40);
-  driveRightBack.move(-40);
-  driveLeftFront.move(-40);
-  driveLeftBack.move(-40);
-  pros::delay(1300);
-  driveRightFront.move(0);
-  driveRightBack.move(0);
-  driveLeftFront.move(0);
-  driveLeftBack.move(0);
-  drivePID(500);
-  turnPIDTime(-200, 700);
-  driveRightFront.move(0);
-  driveRightBack.move(0);
-  driveLeftFront.move(0);
-  driveLeftBack.move(0);
-  pros::delay(200);
-  catapult.move(127);
-  catapult2.move(127);
-  pros::delay(400);
-  catapult.move(0);
-  catapult2.move(0);
-  pros::delay(200);
-  turnPID(900+272);
-  drivePID(3120);
-  turnPID(-900);
-  drivePID(3900);
+
+
 }
 void red_front_nopark_auton() {
   drivePID(3200);
@@ -317,7 +319,7 @@ void red_front_nopark_auton() {
 
   turnPID(1800);
   catapult.move(127);
-  catapult2.move(127);
+  catapult2.move(-127);
   pros::delay(400);
   catapult.move(0);
   catapult2.move(0);
@@ -363,7 +365,7 @@ void blue_front_nopark_auton() {
   drivePID(-2810);
   intake.move(0);
   catapult.move(127);
-  catapult2.move(127);
+  catapult2.move(-127);
   pros::delay(400);
   catapult.move(0);
   catapult2.move(0);
@@ -408,7 +410,7 @@ void red_front_mid_nopark_auton() {
 
   turnPID(1800);
   catapult.move(127);
-  catapult2.move(127);
+  catapult2.move(-127);
   pros::delay(400);
   catapult.move(0);
   catapult2.move(0);
@@ -449,7 +451,7 @@ void prog_skills() {
 
   turnPID(1800);
   catapult.move(127);
-  catapult2.move(127);
+  catapult2.move(-127);
   pros::delay(400);
   catapult.move(0);
   catapult2.move(0);
@@ -466,20 +468,29 @@ void prog_skills() {
   drivePID(7000);
 }
 
-void prog_skills_1() {
-  //red primary auton
-  //basically just red primary with extended drive
-  // line up with the left side of the tile
-drivePID(3200);
-/*
-  drivePID(3200);
-  intake.move(-127);
-  pros::delay(300);
-  drivePID(-500, 900, 0);
-  intake.move(127);
-  drivePID(800, 250, 0);
-  intake.move(0);
-  */
+void testing() {
+
+  auto myChassis = okapi::ChassisControllerFactory::create(
+  {-2, -4}, // Left motors
+  {1, 3},   // Right motors
+  okapi::AbstractMotor::gearset::green,
+  {4_in, 12.5_in} // 4 inch wheels, 12.5 inch wheelbase width
+);
+
+  auto profileController = okapi::AsyncControllerFactory::motionProfile(
+  1.0,  // Maximum linear velocity of the Chassis in m/s
+  2.0,  // Maximum linear acceleration of the Chassis in m/s/s
+  10.0, // Maximum linear jerk of the Chassis in m/s/s/s
+  myChassis  // Chassis Controller
+ );
+  profileController.generatePath({okapi::Point{0_ft, 0_ft, 0_deg}, okapi::Point{4_ft, 0_ft, 0_deg}}, "A");
+  profileController.generatePath({okapi::Point{0_ft, 0_ft, 0_deg}, okapi::Point{4_ft, 0_ft, 0_deg}}, "B");
+  profileController.setTarget("A");
+  profileController.waitUntilSettled();
+  profileController.setTarget("B");
+  profileController.waitUntilSettled();
+
+
 }
 
 void autonomous() {
@@ -508,7 +519,7 @@ void autonomous() {
       prog_skills();
       break;
     case 7:
-      prog_skills_1();
+      testing();
       break;
 }
 
--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -34,7 +34,7 @@ void catapultAutomation(void* param) {
  			}
       if (!catapultIsPrimed && !aborted) {
 				catapult.move(127);
-				catapult2.move(127);
+				catapult2.move(-127);
       }
 			if(catapultIsPrimed) {
 				catapult.move(0);
@@ -44,7 +44,7 @@ void catapultAutomation(void* param) {
 			}
       if(master.get_digital(DIGITAL_L2) && catapultIsPrimed) {
 			catapult.move(127);
-			catapult2.move(127);
+			catapult2.move(-127);
          pros::delay(400);
       }
 		catapultIsPrimed = !(potCatapult.get_value() < 3050);
@@ -59,28 +59,38 @@ void catapultAutomation(void* param) {
 void opcontrol() {
 	pros::Task task_catapult_automation(catapultAutomation);
 	pros::ADIDigitalIn pin1 (1);
+	int strafeTimer = 0;
+	bool broke = false;
 	while (true) {
-   	//pros::lcd::print("potentiometer: %d", potCatapult.get_value());
 		int turn = master.get_analog(ANALOG_LEFT_X);
+		if (master.get_analog(ANALOG_RIGHT_X) > 2) {
+			turn = turn + 11;
+		} else if (master.get_analog(ANALOG_RIGHT_X) < -1) {
+			turn = turn - 11;
+		}
 		int power = master.get_analog(ANALOG_LEFT_Y);
 		int left = power + turn;
 		int right = power - turn;
 		//change to move_velocity?
 
-		if (abs(turn) + abs(power) < 1) {
+
+
+		if (abs(turn) + abs(power) < 1 && !broke) {
 			driveRightFront.move(0);
 			driveRightBack.move(0);
 			driveLeftFront.move(0);
 			driveLeftBack.move(0);
-			driveRightFront.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-			driveRightBack.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-			driveLeftFront.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-			driveLeftBack.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+			driveRightFront.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
+			driveRightBack.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
+			driveLeftFront.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
+			driveLeftBack.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
+			broke = true;
 		} else {
 			driveRightFront.move(right);
 			driveRightBack.move(right);
 			driveLeftFront.move(left);
 			driveLeftBack.move(left);
+			broke = false;
 		}
 
 
@@ -100,20 +110,20 @@ void opcontrol() {
 
 
 		if (master.get_digital(DIGITAL_R1)) {
-			intake.move_velocity(600);
+			intake.move_velocity(-600);
 			// This is 100 because it's a 100rpm motor
 		}
 		else if (master.get_digital(DIGITAL_R2)) {
-			intake.move_velocity(-600);
+			intake.move_velocity(600);
 		}
 		else {
 			intake.move_velocity(0);
 		}
-		/*
+
 		if(master.get_digital_new_press(DIGITAL_Y)) {
 			autonomous();
 		}
-		*/
+
 		switch(getAutonNumber()) {
 		  case 0:
 		    pros::lcd::print(0, "Auton: RED FRONT PARK");
